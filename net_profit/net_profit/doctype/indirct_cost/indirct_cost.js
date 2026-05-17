29988d5fdb51f9e9539cd3a980c754dae132da37
connect4systems <connect4systems@gmail.com>
2026-05-17 15:16:07 +0300

    1st

---
+++ net_profit/net_profit/doctype/indirct_cost/indirct_cost.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Net Profit and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Indirct Cost", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ net_profit/net_profit/doctype/indirct_cost/test_indirct_cost.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Net Profit and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestIndirctCost(FrappeTestCase):
+	pass
